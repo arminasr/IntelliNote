b1e67ccd4f7684641cd8cdf49214670b809cce17
Arminas Ruzgas <arminas.ruzgas@revelsystems.com>
2021-01-20 07:15:18 +0200

    started implementing ui

--- IntelliNote/ContentView.swift
+++ IntelliNote/ContentView.swift
@@ -8,9 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var text = "tekstux"
+    @State private var notes = ["note 1", "another note", "awesome note", "not so awesome note", "the best note"]
+    
+    private var columns: [GridItem] =
+             Array(repeating: .init(.flexible()), count: 1)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            TextField("Take your note", text: $text)
+                .background(Color.red)
+             ScrollView {
+                 LazyVGrid(columns: columns) {
+                    ForEach(notes, id: \.self) { note in
+                        Text(note)
+                    }
+                 }
+                 .font(.headline)
+             }
+            .background(Color.yellow)
+        }
     }
 }
 
