64d1fffc89ed3dd8ba44474331c4c351224f5677
Arminas Ruzgas <arminas.ruzgas@revelsystems.com>
2021-01-20 06:33:26 +0200

    Initial Commit

---
+++ IntelliNote/IntelliNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntelliNoteApp.swift
+//  IntelliNote
+//
+//  Created by Arminas Ruzgas on 2021-01-20.
+//
+
+import SwiftUI
+
+@main
+struct IntelliNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
